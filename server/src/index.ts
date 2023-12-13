25a50080ede9c12583b6f3233bd497e849dcf765
NoodleSushim <ytmikesstuffs@gmail.com>
2023-12-13 16:44:38 +0800

    Add dependencies and configure CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,47 @@
-import express, { Express } from "express";
 import dotenv from "dotenv";
-import mysql from "mysql2";
-import SQL from "sql-template-strings";
-import db from "./models/db";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
+const SERVER_PORT: number = Number(process.env.SERVER_PORT) || 3000;
+
+
+// setup .env
 dotenv.config();
 
-db.query(SQL`SELECT * FROM test`, (err, result) => {
-  console.log(result);
-})
+// setup express
+const app = express();
 
+// setup body-parser
+app.use(bodyParser.urlencoded({extended:true}));
 
-const app: Express = express();
-const SERVER_PORT: number = Number(process.env.SERVER_PORT) || 3000;
+// setup cors
+app.use(cors({
+  origin: [String(process.env.CORS_ORIGIN)],
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
+app.use((req, res, next)=>{
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}); 
+
+const allowedOrigins = [process.env.CORS_ORIGIN];
+app.use(cors({
+  origin: function(origin, callback){
+      if(!origin || allowedOrigins.indexOf(origin) !== -1){
+          callback(null,true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+}));
+
+// setup express to receive json
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
