5b76ba4c73378f864c00a39831bf1801d3396dd3
NoodleSushim <ytmikesstuffs@gmail.com>
2023-12-13 17:26:19 +0800

    Simplify CORS configuration in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,26 +18,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 // setup cors
 app.use(cors({
   origin: [String(process.env.CORS_ORIGIN)],
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-}));
-
-app.use((req, res, next)=>{
-  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-}); 
-
-const allowedOrigins = [process.env.CORS_ORIGIN];
-app.use(cors({
-  origin: function(origin, callback){
-      if(!origin || allowedOrigins.indexOf(origin) !== -1){
-          callback(null,true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
 }));
 
 // setup express to receive json
