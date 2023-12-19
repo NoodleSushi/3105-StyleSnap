96fbcfb37d800f1319dbae4f9a6e3fc049546641
NoodleSushim <ytmikesstuffs@gmail.com>
2023-12-19 08:49:09 +0800

    Change wardrobeRoutes endpoints

--- server/src/routes/wardrobeRoutes.ts
+++ server/src/routes/wardrobeRoutes.ts
@@ -12,7 +12,7 @@ wardrobeRoutes.get('/wardrobes/:wardrobeId', authTokenValidator(), attachUser(),
 wardrobeRoutes.put('/wardrobes/:wardrobeId', authTokenValidator(), attachUser(), wardrobeIdParamValidator(), wardrobeValidator(), updateWardrobe);
 wardrobeRoutes.delete('/wardrobes/:wardrobeId', authTokenValidator(), attachUser(), wardrobeIdParamValidator(), deleteWardrobe);
 
-wardrobeRoutes.get('/wardrobes/user', authTokenValidator(), attachUser(), getWardrobes("user"));
-wardrobeRoutes.get('/wardrobes/user/:userId', authTokenValidator(), attachUser("admin"), userIdParamValidator(), getWardrobes("param"));
+wardrobeRoutes.get('/user/wardrobes', authTokenValidator(), attachUser(), getWardrobes("user"));
+wardrobeRoutes.get('/user/:userId/wardrobes', authTokenValidator(), attachUser("admin"), userIdParamValidator(), getWardrobes("param"));
 
 export default wardrobeRoutes;
