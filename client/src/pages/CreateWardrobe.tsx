276abc52b853e83933c43b62a311f08b4c43567f
Bessa <bessatamarra@gmail.com>
2023-12-20 17:21:08 +0800

    updated navbar and create wardrobe

--- client/src/pages/CreateWardrobe.tsx
+++ client/src/pages/CreateWardrobe.tsx
@@ -1,12 +1,162 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/TopNav';
+import ClothingItem from '../components/ClothingItem';
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  max-width: 100%;
+  flex: 1;
+`;
+
+const LeftColumn = styled.div`
+  width: 32%;
+  padding: 3rem;
+`;
+
+const RightColumn = styled.div`
+  flex: 1;
+  padding: 3rem;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap; /* Allow flex items to wrap to the next row */
+  justify-content: space-between; /* Distribute items evenly along the row */
+  width: 100%; /* Adjusted width to take up half of the page */
+`;
+
+const Dropdown = styled.select`
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 5px;
+`;
+
+const VerticalMenu = styled.div`
+  margin-bottom: 2rem;
+  width: 80%;
+  padding: 2rem;
+`;
+
+const MenuItem = styled.div`
+  cursor: pointer;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* Add this line to align content horizontally */
+
+  &:hover {
+    background-color: #A4A4A4; /* Change to the desired hover background color */
+    border-radius: 0.2rem;
+  }
+`;
+
+const MenuArrow = styled.div<{ isOpen: boolean }>`
+  margin-left: auto;
+  transform: ${({ isOpen }) => (isOpen ? 'rotate(90deg)' : 'rotate(0deg)')};
+  transition: transform 0.3s ease;
+`;
+
+const CollapsibleContent = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  overflow: auto; /* Change this line to make the content scrollable */
+  max-height: 200px; /* Adjust the max height as needed */
+`;
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+`;
 
 const CreateWardrobe: React.FC = () => {
+  const [selectedWardrobe, setSelectedWardrobe] = useState<string>('wardrobe1');
+
+  // Sample data for ClothingItem components
+  const clothingItems = [
+    { imageUrl: 'your_image_url_1.jpg', itemName: 'Item 1', showRemoveButton: false },
+    { imageUrl: 'your_image_url_2.jpg', itemName: 'Item 2', showRemoveButton: false },
+    // Add more items as needed
+  ];
+
+  const [menuItems, setMenuItems] = useState([
+    { category: 'Outerwear', isOpen: false, cards: ['Card 1', 'Card 2'] },
+    { category: 'Tops', isOpen: false, cards: ['Card 3', 'Card 4'] },
+    { category: 'Bottoms', isOpen: false, cards: ['Card 5', 'Card 6'] },
+    { category: 'Footwear', isOpen: false, cards: ['Card 7', 'Card 8'] },
+    { category: 'Accessories', isOpen: false, cards: ['Card 9', 'Card 10'] },
+    { category: 'Shoes', isOpen: false, cards: ['Card 11', 'Card 12'] },
+  ]);
+
+  const toggleMenu = (index: number) => {
+    setMenuItems((prevItems) => {
+      const updatedItems = prevItems.map((item, i) => ({
+        ...item,
+        isOpen: i === index ? !item.isOpen : item.isOpen,
+      }));
+      return updatedItems;
+    });
+  };
+
+  const handleCardClick = (card: string) => {
+    // Your logic for handling card click
+    console.log(`Clicked on ${card}`);
+  };
+
+  const handleRemoveCard = (card: string) => {
+    // Your logic for handling card removal
+    console.log(`Remove ${card}`);
+  };
+
   return (
-    <div>
+    <PageContainer>
       <Navbar />
-      <h1>Create Wardrobe</h1>
-    </div>
+
+      <ContentContainer>
+        <LeftColumn>
+          {/* Dropdown for selecting between Wardrobe 1 and 2 */}
+          <Dropdown onChange={(e) => setSelectedWardrobe(e.target.value)}>
+            <option value="wardrobe1">Wardrobe 1</option>
+            <option value="wardrobe2">Wardrobe 2</option>
+          </Dropdown>
+        </LeftColumn>
+
+        <RightColumn>
+
+          {/* Vertical Menu replicated from CreateOutfit */}
+          <VerticalMenu>
+            {menuItems.map((item, index) => (
+              <div key={index}>
+                <MenuItem onClick={() => toggleMenu(index)}>
+                  {item.category}
+                  <MenuArrow isOpen={item.isOpen}>▶</MenuArrow>
+                </MenuItem>
+
+                <CollapsibleContent isOpen={item.isOpen}>
+                  <RowContainer>
+                    {item.cards.map((card, cardIndex) => (
+                      <ClothingItem
+                        key={cardIndex}
+                        imageUrl={`your_image_url_${cardIndex + 1}.jpg`}
+                        itemName={card}
+                        onClick={() => handleCardClick(card)}
+                        onRemove={() => handleRemoveCard(card)}
+                        showRemoveButton={false}
+                      />
+                    ))}
+                  </RowContainer>
+                </CollapsibleContent>
+              </div>
+            ))}
+          </VerticalMenu>
+        </RightColumn>
+      </ContentContainer>
+    </PageContainer>
   );
 };
 
