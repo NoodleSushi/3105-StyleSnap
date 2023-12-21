abe96d547b535fc7d5f8daf93ff4d68816e60021
NoodleSushi <ytmikesstuffs@gmail.com>
2023-12-21 09:46:25 +0800

    Remove unused route from authRoutes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { userAuthValidator, userLoginValidator, authTokenValidator } from "../validators/authValidator";
-import { attachUser, authTest, createUserAccount, loginUser } from "../controllers/authController";
+import { createUserAccount, loginUser } from "../controllers/authController";
 
 
 const authRoutes = Router();
 
 authRoutes.post('/signup', userAuthValidator(), createUserAccount);
 authRoutes.post('/login', userLoginValidator(), loginUser);
-authRoutes.post('/authtest', authTokenValidator(), attachUser(), authTest);
 
 export default authRoutes;
