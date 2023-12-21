5acae5f0eb2ca1c3ec636cf332613365438a46c1
NoodleSushi <ytmikesstuffs@gmail.com>
2023-12-21 11:22:01 +0800

    Add MulterError handling in validateMulterUpload function

--- server/src/validators/generalValidator.ts
+++ server/src/validators/generalValidator.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express-serve-static-core";
 import { param } from "express-validator";
 import { statusClientBadRequestError } from "../controllers/responseGenerators";
+import { MulterError } from "multer";
 
 export const userIdParamValidator = () => param("userId")
   .notEmpty()
@@ -8,9 +9,9 @@ export const userIdParamValidator = () => param("userId")
   .isInt({ min: 1 })
   .withMessage("User ID must be a positive integer.");
 
-export const validateMulterUpload: (upload: RequestHandler) => RequestHandler = (upload) => (req, res, next) => {
+export const validateMulterUpload: (upload: RequestHandler, optional?: boolean) => RequestHandler = (upload, optional = false) => (req, res, next) => {
   upload(req, res, (err: any) => {
-    if (err) {
+    if (err instanceof MulterError && !optional) {
       return statusClientBadRequestError(res, err.message);
     }
     next();
